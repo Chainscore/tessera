51f934762baba0e810af5f5da0250dcf5265cdc4
Harsh Arora <harsh@chainscore.finance>
2025-02-14 14:36:07 +0530

    merklization: types fixes

--- jam/merklization/binary_merkle.py
+++ jam/merklization/binary_merkle.py
@@ -1,30 +1,39 @@
-from typing import Optional
+from typing import Optional, Callable
 
 from jam.types import Vector
 from jam.types.base.sequences.bytes import ByteArray32
 from jam.types.protocol.crypto import Hash
+# from jam.types.base.sequences.bytes.bytes import Bytes
 # import math
 
 HashType = ByteArray32
 
-def _preprocessor_fn(values: Vector[ByteArray32], hash_fn: Optional[Hash] = Hash.sha256) -> Vector[HashType]:
-    """Constancy Preprocessor Function Implementation as defined in Equation E.7 in Section E.1.2"""
-    new_values: Vector[HashType] = Vector()
-
-    for val in values:
-        new_val = hash_fn('leaf' + val)
-        new_values.append(new_val)
-
-    return new_values
 
 class BinaryMerkle:
     """General Merklization implementation for Binary Trees as defined in Section E.1"""
 
     def __init__(self):
         self._ZERO_HASH = ByteArray32([0] * 32)
+        self._NODE_PREFIX = bytes('node', 'utf-8')
+        self._LEAF_PREFIX = bytes('leaf', 'utf-8')
 
-    def _node_fn(self, values: Vector[ByteArray32], hash_fn: Optional[Hash] = Hash.sha256) -> HashType:
+    def _preprocessor_fn(self, values: Vector[ByteArray32], hash_fn: Optional[Hash]) -> Vector[HashType]:
+        """Constancy Preprocessor Function Implementation as defined in Equation E.7 in Section E.1.2"""
+        if not hash_fn:
+            hash_fn = Hash.sha256
+
+        new_values: Vector[HashType] = Vector()
+
+        for val in values:
+            new_val = hash_fn(self._LEAF_PREFIX + bytes(val))
+            new_values.append(new_val)
+
+        return new_values
+
+    def _node_fn(self, values: Vector[ByteArray32], hash_fn: Optional[Hash]) -> HashType:
         """Node Function Implementation as defined in Equation E.1"""
+        if not hash_fn:
+            hash_fn = Hash.sha256
 
         if len(values) == 0:
             return self._ZERO_HASH
@@ -33,17 +42,21 @@ class BinaryMerkle:
         else:
             left = values[:len(values) // 2]
             right = values[len(values) // 2:]
-            return hash_fn('node' + self._node_fn(left, hash_fn) + self._node_fn(right, hash_fn))
+            return hash_fn(self._NODE_PREFIX + bytes(self._node_fn(left, hash_fn)) + bytes(self._node_fn(right, hash_fn)))
 
-    def wb_merkle_fn(self, values: Vector[ByteArray32], hash_fn: Optional[Hash] = Hash.sha256) -> HashType:
+    def wb_merkle_fn(self, values: Vector[ByteArray32], hash_fn: Optional[Hash]) -> HashType:
         """Well Balanced Binary Merkle Function Implementation as defined in Equation E.3 in Section E.1.1"""
+        if not hash_fn:
+            hash_fn = Hash.sha256
 
         if len(values) == 1:
             return hash_fn(values[0])
         else:
             return self._node_fn(values, hash_fn)
 
-    def cd_merkle_fn(self, values: Vector[ByteArray32], hash_fn: Optional[Hash] = Hash.sha256) -> HashType:
+    def cd_merkle_fn(self, values: Vector[ByteArray32], hash_fn: Optional[Hash]) -> HashType:
         """Constant Depth Binary Merkle Function Implementation as defined in Equation E.4 in Section E.1.2"""
+        if not hash_fn:
+            hash_fn = Hash.sha256
 
-        return self._node_fn(_preprocessor_fn(values, hash_fn), hash_fn)
+        return self._node_fn(self._preprocessor_fn(values, hash_fn), hash_fn)
