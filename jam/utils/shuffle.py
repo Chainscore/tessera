ca6b77ae9f036a04503b11b84017618204ad9da9
dikshant441-chainscore <dikshant@chainscore.finance>
2025-02-17 18:04:35 +0530

    module/shuffling

--- jam/utils/shuffle.py
+++ jam/utils/shuffle.py
@@ -6,12 +6,32 @@ from jam.utils.codec.primitives.integers import encode, decode
 class U32Vector(Vector): ...
 
 def fisher_yates_with_hash(array:U32Vector, l:int, random_array):
+    """
+    Description:
+        This function takes three arguments: an array, the length of the array, and a random_array. It shuffles the given array based on the indices provided in random_array and returns the shuffled array.
+    Args:
+        array: The array that needs to be shuffled.
+        l: The length of the array that is going to be shuffled.
+        random_array : An array of the same length as the original array, generated using a given hash, which determines the shuffle order.
+
+    Returns:
+        Returns a shuffled array of integers.
+    """
     for i in range(0, l):
         j = random_array[i] % (l - i)
         array[l - i - 1], array[j] = array[j], array[l - i - 1]
 
 
 def get_random(h, l):
+    """
+    Description:
+        The `get_random` function helps generate a `random_array` with the same length as the array to be shuffled. It takes a hash value, performs some operations on it, and generates a new `random_array`.
+    Arg:
+        h: hash value
+        l: length of array(array to be shuffled)
+
+    Return:
+        A random_array that is generated using a hash value after performing certain operations on the hash value.    """
     random = []
     for i in range(l):
         new_h = h + encode(i // 8, 4)
@@ -33,12 +53,4 @@ def shuffle(h, array: U32Vector) -> U32Vector:
     array.reverse()
     return array
 
-# def main():
-#     array:U32Vector = U32Vector([])
-#     for i in range(0, 341):
-#         array.append(U32(i))
-#
-#     numbers = shuffle('d111a554e3e8a058ea18c05bc943fa3cad8fb1339bf9307f2f3d9228ae5c934b', array)
-#     print(numbers)
-#
-# main()
+
