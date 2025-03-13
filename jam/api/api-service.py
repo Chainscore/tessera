5bb77e734fe3625fe1ad08a024dc002d4b4ea5cf
pranjal-chainscore <pranjal@chainscore.finance>
2025-03-13 15:41:15 +0530

    added safarole state validation

--- jam/api/api-service.py
+++ jam/api/api-service.py
@@ -1,39 +1,49 @@
-from typing import Union
+from typing import Union, Optional
 from fastapi import FastAPI
-from markdown_it.rules_core import block
+# from markdown_it.rules_core import block
 from pydantic import BaseModel
 import json
 from typing import Any, Dict, List
-
 from requests import Request
-
+from jam.consensus.safrole.safrole import  Safrole
+from jam.state.state import State
 from jam.statistics.statistics import Statistics
 from yarl import Query
-
 from jam.types import Boolean
 from tests.unit.statistics.test_statistics_stf import create_block_from_input
 from tests.unit.statistics.test_statistics_stf import create_state_from_pre
 from tests.unit.statistics.test_statistics_stf import get_testcases_starting_with
-from tests.unit.statistics.types import Testcase
+from tests.unit.statistics.types import Testcase, PreState
+from jam.types.block import Block
+from jam.consensus.safrole.safrole import Safrole
+from jam.recent_history.recent_history import RecentHistory
 
 app = FastAPI()
 
-# async def get_json(request:  Request):
-#     try:
-#         raw_json = await request.json()
-#         return raw
-@app.get("/statistics")
-async def read_item( block_data : str):
-    # data = json.loads
+# json format
+# json={
+#         "input": {"block": input_data, "state": pre_state},
+#         "output": {"state": post_state},
+#         "flags": {"size": "tiny", "active": True}
+#     }
+class InputData(BaseModel):
+    block : Any
+    state : Any
 
+class RequestData(BaseModel):
+    input: InputData
+
+    print("Runs")
+
+@app.post("/api/v1/safrole/validate")
+async def vaildate_safarole(request_data: RequestData):
+    # data = json.loads
     try:
-        data = json.loads(block_data)
-        vector = Testcase.from_json(data)
-        test_block = create_block_from_input(vector.input)
-        test_state = create_state_from_pre(vector.pre_state)
-        output = Statistics.transition(test_state, test_block)
-        assert output.pi[0] == vector.post_state.pi.current
-        assert output.pi[1] == vector.post_state.pi.last
+
+        test_block = Block.from_json(request_data.input.block)
+        # test_state = PreState.from_json(request_data.input.state)
+        # state_from_prestate = create_state_from_pre(test_state)
+        output = Safrole.transition(request_data.input.state, test_block)
 
     except Exception as e:
         print("Failed XXX", e)
