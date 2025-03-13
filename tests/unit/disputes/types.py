5e9a89117ce25ef8b6f0d1034cad5068cf2a0ad8
Harsh Arora <harsh@chainscore.finance>
2025-03-13 15:25:52 +0530

    disputes: testcases dir fix

--- tests/unit/disputes/types.py
+++ tests/unit/disputes/types.py
@@ -48,8 +48,8 @@ class Testcase(Codable,JsonSerde):
 
 
 def get_testcases_starting_with(prefix: str = "", limit: int = 10) -> List[Testcase]:
-    # data_dir = "tests/unit/disputes/data/tiny"
-    data_dir = "./data/tiny"
+    data_dir = "tests/unit/disputes/data/tiny"
+    # data_dir = "./data/tiny"
     result = []
     for index, file in enumerate(os.listdir(data_dir)):
         if len(result) >= limit:
