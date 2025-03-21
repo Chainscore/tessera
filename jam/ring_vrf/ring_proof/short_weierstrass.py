43a7acc19ac5aafbcc04bbc59f5cb0818d186c65
konasiva7 <sivakona@chainscore.finance>
2025-03-21 13:38:52 +0530

    updated short_wierstrass

--- jam/ring_vrf/ring_proof/short_weierstrass.py
+++ jam/ring_vrf/ring_proof/short_weierstrass.py
@@ -1,12 +1,10 @@
 from jam.ring_vrf.curve.specs.bandersnatch import BandersnatchParams
 from sympy import mod_inverse
-from jam.ring_vrf.ring_proof.constants import SeedPoint, PaddingPoint, Blinding_Base
+from jam.ring_vrf.ring_proof.constants import SeedPoint, PaddingPoint, Blinding_Base, S_A, S_B, S_PRIME
 
 # from jam.ring_vrf.ring_proof.short_weierstrass_curve_ops import point_multiplication
-
 # from jam.ring_vrf.ring_proof.short_weierstrass_curve_ops import point_addition, point_multiplication, point_doubling
 
-S_PRIME_FIELD=BandersnatchParams.PRIME_FIELD
 
 mont_a=29978822694968839326280996386011761570173833766074948509196803838190355340952#2*(BandersnatchParams.EDWARDS_A + BandersnatchParams.EDWARDS_D) % BandersnatchParams.ORDER * mod_inverse(BandersnatchParams.EDWARDS_A-BandersnatchParams.EDWARDS_D, BandersnatchParams.PRIME_FIELD) % BandersnatchParams.ORDER
 
@@ -28,52 +26,44 @@ def twisted_edward_to_mont(point):
     output: Montgomery Point
     """
     x,y=point
-    mont_x=(1+y) * mod_inverse(1-y,S_PRIME_FIELD) % S_PRIME_FIELD
+    mont_x=(1+y) * mod_inverse(1-y,S_PRIME) % S_PRIME
 
-    mont_y= (1+y)* mod_inverse(x- (x* y),S_PRIME_FIELD) % S_PRIME_FIELD
-    return  mont_x,mont_y
-
-
-
-ws_a=10773120815616481058602537765553212789256758185246796157495669123169359657269
-ws_b=29569587568322301171008055308580903175558631321415017492731745847794083609535
+    mont_y= (1+y)* mod_inverse(x- (x* y),S_PRIME) % S_PRIME
+    return  int(mont_x),int(mont_y)
 
 
 def mont_to_short_weierstrass(mont_point):
     # // ((x + A / 3) / B, y / B)
     x,y=mont_point
-    x_sw= (x +  mont_a % S_PRIME_FIELD * mod_inverse(3,S_PRIME_FIELD) %S_PRIME_FIELD )* mod_inverse(mont_b,S_PRIME_FIELD) %S_PRIME_FIELD
-    y_sw=y* mod_inverse(mont_b,S_PRIME_FIELD)% S_PRIME_FIELD
-    return x_sw,y_sw
+    x_sw= (x +  mont_a % S_PRIME * mod_inverse(3,S_PRIME) %S_PRIME )* mod_inverse(mont_b,S_PRIME) %S_PRIME
+    y_sw=y* mod_inverse(mont_b,S_PRIME)% S_PRIME
+    return int(x_sw),int(y_sw)
 
 
 def is_on_weierstrass(point):
     x,y=point
     #y^2=x^3+a*x+b
-    lhs=pow(y,2,S_PRIME_FIELD)
-    rhs=(pow(x,3,S_PRIME_FIELD) + ws_a * x  % S_PRIME_FIELD + ws_b % S_PRIME_FIELD) % S_PRIME_FIELD
+    lhs=pow(y,2,S_PRIME)
+    rhs=(pow(x,3,S_PRIME) + S_A * x  % S_PRIME + S_B % S_PRIME) % S_PRIME
     # print("weierstrass:")
     # print(lhs)
     # print(rhs)
-    # print(lhs>S_PRIME_FIELD)
-    # print(rhs>S_PRIME_FIELD)
+    # print(lhs>S_PRIME)
+    # print(rhs>S_PRIME)
     return lhs==rhs
 
 
 def is_on_monty(point):
     x,y=point
     # B*y**2=x**3+a*x**2+x
-    lhs= mont_b *pow(y,2,S_PRIME_FIELD) % S_PRIME_FIELD
-    rhs= (pow(x,3,S_PRIME_FIELD) % S_PRIME_FIELD + mont_a *pow(x,2,S_PRIME_FIELD) % S_PRIME_FIELD +x % S_PRIME_FIELD ) % S_PRIME_FIELD
+    lhs= mont_b *pow(y,2,S_PRIME) % S_PRIME
+    rhs= (pow(x,3,S_PRIME) % S_PRIME + mont_a *pow(x,2,S_PRIME) % S_PRIME +x % S_PRIME ) % S_PRIME
     # print("monty:")
-    # print(lhs>S_PRIME_FIELD)
-    # print(rhs>S_PRIME_FIELD)
+    # print(lhs>S_PRIME)
+    # print(rhs>S_PRIME)
     # print(lhs)
     # print(rhs)
     return lhs==rhs
-
-
-
 
 # print(PaddingPoint)
 # print("TE_2_Mont:",twisted_edward_to_mont(PaddingPoint))
@@ -82,26 +72,26 @@ def is_on_monty(point):
 # print("IS ON SW:",is_on_weierstrass(twisted_edward_to_sw(PaddingPoint)))
 
 
-
 ##SHORT WEIERSTRASS TO TWISTED_EDWARD##
 
 def short_ws_to_monty(sw_point):
     ws_x,ws_y=sw_point
     # (Bx - A / 3, By)
-    mont_x= (mont_b* ws_x % S_PRIME_FIELD - mont_a *mod_inverse(3,S_PRIME_FIELD)) % S_PRIME_FIELD
-    mont_y= mont_b * ws_y % S_PRIME_FIELD
-    return mont_x,mont_y
+    mont_x= (mont_b* ws_x % S_PRIME - mont_a *mod_inverse(3,S_PRIME)) % S_PRIME
+    mont_y= mont_b * ws_y % S_PRIME
+    return int(mont_x),int(mont_y)
 
 
 def monty_to_twisted_edward(mont_point):#(x, y) -> (x / y, (x−1) / (x + 1))
     mont_x,mont_y=mont_point
-    te_x= mont_x * mod_inverse(mont_y,S_PRIME_FIELD) % S_PRIME_FIELD
-    te_y= (mont_x -1) * mod_inverse(mont_x+1,S_PRIME_FIELD) % S_PRIME_FIELD
-    return te_x,te_y
+    te_x= mont_x * mod_inverse(mont_y,S_PRIME) % S_PRIME
+    te_y= (mont_x -1) * mod_inverse(mont_x+1,S_PRIME) % S_PRIME
+    return int(te_x),int(te_y)
 
 
 def short_to_te(sw_point):
     mont_point= short_ws_to_monty(sw_point)
+    print(mont_point)
     te_point=monty_to_twisted_edward(mont_point)
     return  te_point
 
@@ -122,7 +112,7 @@ def short_to_te(sw_point):
 # print("TO TE",short_to_te((twisted_edward_to_sw(PaddingPoint))))
 # print("is on monty:", is_on_monty(short_ws_to_monty(twisted_edward_to_sw(PaddingPoint))))
 # print("is same:",short_to_te((twisted_edward_to_sw(PaddingPoint)))==PaddingPoint)
-# print(S_PRIME_FIELD)
+# print(S_PRIME)
 #
 # print(0x4247698f4e32ad45a293959b4ca17afa4a2d2317e4c6ce5023e1fd63d1b5de98)
 #
