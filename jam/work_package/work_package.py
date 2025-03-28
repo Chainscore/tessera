a959be54cb04351e111c51f6906fd8769cce7d47
dikshant441-chainscore <dikshant@chainscore.finance>
2025-03-28 14:55:20 +0530

    wp module and adr file

---
+++ jam/work_package/work_package.py
@@ -0,0 +1,10 @@
+from jam.types.work.item import WorkItem
+from jam.utils.constants import MAX_MANIFEST_ENTRIES
+
+
+class WorkPackage:
+    @staticmethod
+    def segment_count():
+        if WorkItem.export_count <= MAX_MANIFEST_ENTRIES and WorkItem.;
+
+
