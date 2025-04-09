15af41ad79907b02eb6dc71ac6a6c7a4c7be7a32
faizChainscore <faiz@chainscore.finance>
2025-04-09 12:58:46 +0530

    implemented 14.14 functions as per gray paper

--- jam/work_package/work_package.py
+++ jam/work_package/work_package.py
@@ -1,12 +1,15 @@
 from sympy.physics.units import ha
 
 from jam.types import Bytes
-from jam.types.work.item import WorkItem
+from jam.types.work.item import WorkItem, ExtrinsicSpec
 from jam.types.work.package import  WorkPackage
 from jam.utils.constants import MAX_EXPORT_ITEM, MAX_IMPORT_ITEM, EXTRINSIC_COUNT
 from jam.work_package.error import WorkPackagesErrorCode, WorkPackageError
+from jam.types.work.report import WorkExecResult
+from hashlib import blake2b
+from jam.types.base.integers.fixed import U32
 from math import floor
-
+from jam.merklization.binary_merkle import BMRFunctions
 from jam.types.protocol.core import SegmentRoot, WorkPackageHash
 from jam.types.base.dictionary import Dictionary, decodable_dictionary
 from jam.types.protocol.crypto import OpaqueHash
@@ -53,6 +56,40 @@ class WorkPackage:
             for i in range(padding_zero):
                 arr.append(0)
         return arr
+
+    @staticmethod
+    def _ext(w: WorkItem, d):
+        result = []
+        for item in d:
+            first = blake2b(d)
+            second = U32(len(d))
+            if ExtrinsicSpec(hash=first, len=second) in w.extrinsic:
+                result.append(item)
+        return result
+
+    def _imp_seg(self, w: WorkItem, segments):
+        result = []
+        for s in segments:
+            for (r, n) in w.import_segments:
+                merkle = BMRFunctions()
+                if WorkPackage.segment_root_lookup(self, r) == BMRFunctions.cd_merkle_fn(merkle,s):
+                        result.append(s[n])
+        return result
+
+    def _verify_imp(self, w: WorkItem, segments):
+        result = []
+        for s in segments:
+            for (r, n) in w.import_segments:
+                merkle = BMRFunctions()
+                if WorkPackage.segment_root_lookup(self, r) == BMRFunctions.cd_merkle_fn(merkle, s):
+                    result.append(BMRFunctions.merkle_path_fn(merkle, s, len(s), n))
+        return result
+
+
+    @staticmethod
+    def wr_i(p:WorkPackage, j:int):
+        print("utils for wok result computation")
+
 
     def segment_root_lookup(self, r: OpaqueHash) -> SegmentRoot:
         """
