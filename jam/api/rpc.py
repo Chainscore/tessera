1057d2baf9ea820dec5545a2a3e3bf3fd9b90e2e
pranjal-chainscore <pranjal.chainscore@finance.com>
2025-04-26 22:02:28 +0530

    refactored code

--- jam/api/rpc.py
+++ jam/api/rpc.py
@@ -4,16 +4,16 @@ import random
 import tempfile
 from datetime import datetime
 from typing import Any, Dict, Optional, Set
-
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from pydantic import BaseModel
-
 from jam.consensus.grandpa.finality import Finality
 from jam.db.kv import KVStore
 from jam.state.state import State
 from jam.types.block import Block
 from jam.types.header import Header
 from jam.types.protocol.core import TimeSlot
+from jam.types import  U32
+
 
 # Initialize FastAPI with metadata for Swagger UI
 app = FastAPI(
@@ -33,17 +33,14 @@ state.save(db)
 
 # Load the state from the database
 db_state = State.load(db)
-print("header hash to bytes 32", Header.__hash__(db_block.header).to_bytes(32))
-print("header hash", Header.__hash__(db_block.header))
-print("hash bytes to hex", Header.__hash__(db_block.header).to_bytes(32).hex())
-print("hash arrray", list(Header.__hash__(db_block.header).to_bytes(32)))
-# print("Retrieved State:", db_state)
-# print("db_path", db.load())
-
-# print("state", state)
-# print("block", block)
-# print("block stats", state.pi)
-# print("block_hash", Header.__hash__(block.header).to_bytes(32), type (Header.__hash__(block.header).to_bytes(32)))
+# print("header hash to bytes 32", Header.__hash__(db_block.header).to_bytes(32))
+# print("header hash", Header.__hash__(db_block.header))
+# print("hash bytes to hex", Header.__hash__(db_block.header).to_bytes(32).hex())
+# print("hash arrray", list(Header.__hash__(db_block.header).to_bytes(32)))
+# hash_array = list(Header.__hash__(db_block.header).to_bytes(32))
+# hash_bytes = bytes(hash_array)
+# print(hash_bytes)
+# print("db_block.header.slot", db_state)
 
 # Following the etherum json rpc api structure
 """
@@ -113,15 +110,28 @@ async def rpc_handler(request: RpcRequest):
                 },
             )
         if len(params):
-            if params["Hash"] == Header.__hash__(db_block.header):
+
+            if db_block.header.slot == U32(0):
+                return RpcResponse(
+                    jsonrpc="2.0",
+                    id=request.id,
+                    error={
+                    "code": -32602,
+                    "message": "No parent block available for genesis block",
+                },
+                )
+
+            if bytes(params["Hash"]) == Header.__hash__(db_block.header).to_bytes(32):
                 # Return the parent block
                 parent_block = Block.load_parent(db_block.header.slot, db)
+                print("parent_block.header.slot", parent_block.header.slot)
+
                 return RpcResponse(
                     jsonrpc="2.0",
                     id=request.id,
                     result=[
                         Header.__hash__(parent_block.header),
-                        parent_block.header.slot,
+                        int(parent_block.header.slot),
                     ],
                 )
             else:
@@ -152,12 +162,14 @@ async def rpc_handler(request: RpcRequest):
                     "message": "Invalid parameters: expected header_hash",
                 },
             )
-
-        if header_hash == Header.__hash__(db_block.header):
+        
+        if bytes(params["Hash"]) == Header.__hash__(db_block.header).to_bytes(32):
             return RpcResponse(
                 jsonrpc="2.0",
                 id=request.id,
-                result=[db_block.header.parent_state_root.encode()],
+                result=[
+                    bytes(db_block.header.parent_state_root).hex(),
+                    ],
             )
         else:
             return RpcResponse(
@@ -188,9 +200,11 @@ async def rpc_handler(request: RpcRequest):
                 },
             )
 
-        if header_hash == Header.__hash__(db_block.header):
+        if bytes(params["Hash"]) == Header.__hash__(db_block.header).to_bytes(32):
             return RpcResponse(
-                jsonrpc="2.0", id=request.id, result=[db_state.pi.encode()]
+                jsonrpc="2.0", 
+                id=request.id, 
+                result=[db_state.pi.encode()]
             )
         else:
             return RpcResponse(
