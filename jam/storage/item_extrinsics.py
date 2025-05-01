7974654e5d2d17bd22a1085eb95b095769137c0a
prasad-kumkar <prasad@chainscore.finance>
2025-05-01 23:18:06 +0530

    fix: added validation before storing extrinsic, docs: docstrings added

--- jam/storage/item_extrinsics.py
+++ jam/storage/item_extrinsics.py
@@ -6,18 +6,48 @@ from jam.types.work.package import WorkPackage
 
 
 class ItemExtrinsics:
-    @classmethod
-    def store(cls, package: WorkPackage, data: Bytes, db: KVStore):
-        offset = 0
-        for item in package.items:
-            for extrinsic in item.extrinsic:
-                value = data[offset: offset+extrinsic.len]
-                print("val", value)
-                if Hash.blake2b(value) != extrinsic.hash:
-                    raise ValueError("Invalid WP: Extrinsic data mismatch")
-                db.put(bytes(extrinsic.hash), bytes(value))
-                offset += int(extrinsic.len)
+    """Place to store and get extrinsics that are received along with Work Package"""
 
     @classmethod
-    def get(cls, extrinsic_hash: ByteArray32, db: KVStore) -> Bytes:
+    def store(cls, package: WorkPackage, data: Bytes, db: KVStore):
+        """Stores the extrinsic data, decoded as per work items and extrinsic lengths we get from WP
+
+        Args:
+            package (WorkPackage): Relevent WP
+            data (Bytes): Extrinsic data we receive with WP
+            db (KVStore): Data store
+
+        Raises:
+            ValueError: If the extrinsic passed (to store) is invalid - i.e hashes/length dont match 
+        """
+        offset = 0
+        to_store = {}
+        # Validation process
+        for item in package.items:
+            for extrinsic in item.extrinsic:
+                if offset+extrinsic.len > len(data):
+                    raise ValueError("Invalid WP: Extrinsic data mismatch")
+                value = data[offset: offset+extrinsic.len]
+                if Hash.blake2b(value) != extrinsic.hash:
+                    raise ValueError("Invalid WP: Extrinsic data mismatch")
+                offset += int(extrinsic.len)
+                to_store[extrinsic.hash] = value
+        if offset != len(data):
+            raise ValueError("Invalid WP: Extrinsic data mismatch")
+        
+        # Storing data
+        for key, value in to_store.items():
+            db.put(bytes(key), bytes(value))
+
+    @classmethod
+    def get(cls, extrinsic_hash: ByteArray32, db: KVStore) -> bytes:
+        """Gets the relevent extrinsic, assuming we already know its hash (as its stored in WI)
+
+        Args:
+            extrinsic_hash (ByteArray32)
+            db (KVStore)
+
+        Returns:
+            Bytes
+        """
         return db.get(bytes(extrinsic_hash))
