9440c9ebaf1e6ba0f1ad35ca7590cf99d69d8dc3
konasiva7 <sivakona@chainscore.finance>
2025-05-08 17:15:47 +0530

    updated constraint aggregation

--- jam/ring_vrf/ring_proof/constraints_aggregation.py
+++ jam/ring_vrf/ring_proof/constraints_aggregation.py
@@ -1,79 +1,86 @@
-
 import hashlib
 import random
-from py_ecc.optimized_bls12_381 import normalize
 import sys
-from jam.ring_vrf.ring_proof.polynomial_interpolation import poly_scalar, poly_multiply, poly_add
-from jam.ring_vrf.ring_proof.KZG_polynomial_commit_open_verify import commit_to_polynomial
-from jam.ring_vrf.ring_proof.constants import S_PRIME,D
-from jam.ring_vrf.ring_proof.preprocessing import end_time
+from jam.ring_vrf.ring_proof.fiat_shamir_with_transcript import alphas
+from jam.ring_vrf.ring_proof.poly_interpolation_fft import poly_interpolate_fft
+from jam.ring_vrf.ring_proof.polynomial_vect_ops import vect_add, vect_scalar_mul, poly_multiply
+from jam.ring_vrf.ring_proof.constants import S_PRIME, D, omega, omega_2048
 from jam.ring_vrf.ring_proof.prover_witness_polynomials import C_b_v,C_acc_x,C_acc_ip,C_acc_y
-from jam.ring_vrf.ring_proof.constraints_polynomials import c1x, c2x, c3x, c4x, c5x, c6x, c7x
+from jam.ring_vrf.ring_proof.constraints_polynomial_evals import c1x, c2x, c3x, c4x, c5x, c6x, c7x
 
 sys.set_int_max_str_digits(1000000)
 
-def hash_commitments(commitments):
-    """
-    """
-    hasher = hashlib.sha512()
-
-    for commitment in commitments:
-        # Convert projective (X, Y, Z) -> affine (x, y)
-        x, y = normalize(commitment)  # Normalize converts to affine
-        # Convert x, y to bytes and update hash
-        hasher.update(x.n.to_bytes(48, 'little'))  # Convert field element to bytes
-        hasher.update(y.n.to_bytes(48, "little"))
-
-    return int.from_bytes(hasher.digest(), 'little')
-
-
-def generate_alpha(commitments, num_alphas=7):
-    """
-    """
-    seed = hash_commitments(commitments)  # Get deterministic seed from commitments
-    random.seed(seed)
-    # Seed PRNG for deterministic outputs
-    return [random.randint(1, S_PRIME) for _ in range(num_alphas)]  # Generate alpha values
-
-
-
-def construct_aggregation_constraint(alphas, constraints, domain):
-    """
-    """
-    # x = 1
-    c_x=[0]
-    # Compute the weighted sum of constraints
-    for i in range(len(alphas)):
-
-        c_x=poly_add(poly_scalar(constraints[i],alphas[i],S_PRIME),c_x,S_PRIME)
-
-    # Multiply by (x - ω^(N-3)) (x - ω^(N-2)) (x - ω^(N-1))
-    # print('modulo cx')
-   #vanishing term
-    vanishing_term=[1]
-    for k in range(3):
-        vanishing_term= poly_multiply(vanishing_term,[-domain[0-k],1],S_PRIME)#x-W^N-k (W^N-1 is the last element of D)
-
-    # Final aggregated constraint
-    c_x = poly_multiply(c_x,vanishing_term,S_PRIME)
-    # print('to do mod after v t')
-    return c_x
-
-#Get the apa values from the Prover's commitments
-commitments = [C_b_v,C_acc_x,C_acc_y,C_acc_ip]
 constraints=[c1x,c2x,c3x,c4x,c5x,c6x,c7x]
+commitments = [C_b_v,C_acc_x,C_acc_y,C_acc_ip]
 
-# alpha_values = generate_alpha(commitments)
-# alpha_values=[alpha% S_PRIME for alpha in alpha_values]
-alpha_values=[23426295910140870709614097412433960347746242342772840884066757906985100343590, 46433544455077235545632682909406958794646428824021350807274305193686650751236, 78101732610314093593150729898315267434854171850795179589607409285815531790, 593197255987830876876009860956149019051956889376694517098490195230307214182, 32175196951432882449291708537928919219460156628583290699727577517487325664113, 34948741935357743784650851171470586551760506493489743006361967647813201562252, 48766446903835583133423634840534770408834461876682423474505157724475361584745]
-print(alpha_values)
-cs_aggregation=construct_aggregation_constraint(alpha_values,constraints,D)
 
-# substitutions = {x: D[idx], omega_x: D[idx+1], N_m4: D[-5]}
-# final_result = cs_aggregation.subs(substitutions)
+def aggregate_evaluations(polys, coeffs, mod=None):
+    """Aggregate evaluation vectors using scalar coefficients."""
+    assert len(polys) == len(coeffs)
+    result = [0] * len(polys[0])
+    for poly, coeff in zip(polys, coeffs):
+        weighted = vect_scalar_mul(poly, coeff, mod)
+        result = vect_add(result, weighted, mod)
+    return result
 
-print("Alpha Values:",alpha_values)
+
+def generate_alphas(commits, n=7):
+
+    alpha_list=alphas
+    return alpha_list
+
+
+
+def remove_trailing_zeros(arr):
+    i = len(arr) - 1
+    while i >= 0 and arr[i] == 0:
+        i -= 1
+    return arr[:i+1]
+
+
+def mul_with_vanishing_poly(aggregated_constraint, prime):
+    vanishing_term = [1]
+    for k in range(1, 4):
+        vanishing_term = poly_multiply(vanishing_term, [-D[-k], 1],
+                                       prime)  # x-W^N-k (W^N-1 is the last element of D)
+
+    # vanishing_poly = [27611812781829920551290133267575249478648871281233506899293410857719571783635,
+    #                   18588772761366545515353714920513821262872352644173316860176739225353543051128,
+    #                   21856349314143507620628005048545870461349664960990502850349462173747080309275,
+    #                   1]
+
+    print("Vansh:", vanishing_term)
+
+    final_cs_agg= poly_multiply(aggregated_constraint, vanishing_term,prime)
+
+    return final_cs_agg
+
+
+#Get the alpha values from the Prover's commitments
+alpha_values = generate_alphas(commitments, 7)
+cs_aggregation=aggregate_evaluations(constraints,alpha_values,S_PRIME)
+
 print("Constraint Aggregated:", cs_aggregation,len(cs_aggregation))
-# print("final result:",final_result)
+
+Interpolated_poly=poly_interpolate_fft(cs_aggregation, omega_2048,S_PRIME)
+
+print("Interpolated_poly:",Interpolated_poly)
+
+Interpolated_poly=remove_trailing_zeros(Interpolated_poly)
+
+print("Interpolated_poly:",Interpolated_poly)
+print("Alpha Values:",alpha_values)
+
+# print("AGGE RESULT:",cs_aggregation)
+
+#Expected
+# Vector:["", "5291858017394326220327389462579461719080734283449386752065209561883729801330",
+#           "33555693263795842704974598853766205191665487365954216337482772216222023911745",
+#           "8411120379136671107946066988856908149731468707639881684643532326074864901112",
+#           "", "19877707608549581885228899168610643567678711376936252844274555508244350760960",
+
+final_agg_cst=mul_with_vanishing_poly(Interpolated_poly, S_PRIME)
+
+print("Final Agg:", final_agg_cst)
 
 
