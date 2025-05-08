22b633ecc9332cd78ad4901656e9f2d20ff49236
konasiva7 <sivakona@chainscore.finance>
2025-05-08 17:20:27 +0530

    updated proof construction

--- jam/ring_vrf/ring_proof/aggregation_poly_proof_construction.py
+++ jam/ring_vrf/ring_proof/aggregation_poly_proof_construction.py
@@ -1,20 +1,20 @@
 import time
+from jam.ring_vrf.ring_proof.fiat_shamir_with_transcript import cf_vectors
 start_is=time.time()
+from py_ecc.optimized_bls12_381 import normalize
 from jam.ring_vrf.ring_proof.constants import S_PRIME
-from jam.ring_vrf.ring_proof.fiat_shamir_funcs import fiat_shamir_final_aggregation
-from jam.ring_vrf.ring_proof.polynomial_interpolation import poly_add, poly_scalar, poly_evaluate
+from jam.ring_vrf.ring_proof.polynomial_vect_ops import poly_add, poly_scalar, poly_evaluate
 from jam.ring_vrf.ring_proof.quotient_polynomial import P_x_zeta, P_y_zeta, acc_x_zeta, acc_ip_zeta, acc_y_zeta, b_zeta, \
-    s_zeta, C_q, quotient_poly, Evaluation_point_Zeta
+    s_zeta, C_q, quotient_poly, Evaluation_point_Zeta#, Evaluation_point_Zeta
 from jam.ring_vrf.ring_proof.linearization_polynomial import l_agg_at_zeta_omega, l_agg, zeta_omega
-from jam.ring_vrf.ring_proof.KZG_polynomial_commit_open_verify import create_kzg_proof as kzg_open
+from jam.ring_vrf.ring_proof.KZG_polynomial_commit_open_verify import create_kzg_proof as kzg_open, commit_to_polynomial
 from jam.ring_vrf.ring_proof.preprocessing import px_I,py_I,s_v_I
 from jam.ring_vrf.ring_proof.prover_witness_polynomials import acc_x_I,acc_y_I, acc_ip_I, b_v_I
 from jam.ring_vrf.ring_proof.prover_witness_polynomials import C_b_v, C_acc_y, C_acc_x, C_acc_ip
 
+
 #get the v list
-
-
-def v_list(px_zeta, py_zeta, s_zeta, b_zeta, accip_zeta, accx_zeta, accy_zeta, l_zeta_omega, prime=None):
+def coeff_list(px_zeta, py_zeta, s_zeta, b_zeta, accip_zeta, accx_zeta, accy_zeta, l_zeta_omega, prime=None):
     """
     input: poly_evaluations
     output: V list
@@ -24,7 +24,7 @@ def v_list(px_zeta, py_zeta, s_zeta, b_zeta, accip_zeta, accx_zeta, accy_zeta, l
 
     values=[px_zeta, py_zeta, s_zeta, b_zeta, accip_zeta, accx_zeta, accy_zeta, l_zeta_omega]
 
-    V_list=fiat_shamir_final_aggregation(values, 8)
+    V_list=cf_vectors
     return V_list
 
 # get the aggregated poly
@@ -45,8 +45,10 @@ def proof_contents_phi(agg, l, zeta, zeta_omega):
     input:agg_poly, liner_poly, zeta, zeta_omega
     output: Phi_zeta, phi_zeta_omega
     """
-    phi_zeta=kzg_open(agg,zeta)[0] # take only proof
-    phi_zeta_omega=kzg_open(l,zeta_omega)[0] #take only proof
+    phi_zeta, one=kzg_open(agg,zeta) # take only proof
+    print("Agg at zeta in proover side:", one)
+    phi_zeta_omega, two=kzg_open(l,zeta_omega) #take only proof
+    print("L_Zeta_omega in proover side:", two)
     return phi_zeta, phi_zeta_omega
 
 def construct_proof(C_b,C_acc_ip,C_acc_x,C_acc_y,P_x_zeta, P_y_zeta,S_zeta,b_zeta,acc_ip_zeta, acc_x_zeta, acc_y_zeta,C_q,l_zeta_omgea,phi_zeta,phi_zeta_omega):
@@ -59,17 +61,23 @@ def construct_proof(C_b,C_acc_ip,C_acc_x,C_acc_y,P_x_zeta, P_y_zeta,S_zeta,b_zet
 
 
 
-V_LIST=v_list(P_x_zeta,P_y_zeta,s_zeta,b_zeta,acc_ip_zeta, acc_x_zeta,acc_y_zeta,l_agg_at_zeta_omega,S_PRIME)
-print("V_LIST", V_LIST)
+V_LIST=coeff_list(P_x_zeta,P_y_zeta,s_zeta,b_zeta,acc_ip_zeta, acc_x_zeta,acc_y_zeta,l_agg_at_zeta_omega,S_PRIME)
+# print("V_LIST", V_LIST)
 agg_poly=aggregated_poly(px_I,py_I, s_v_I, b_v_I, acc_ip_I, acc_x_I, acc_y_I,quotient_poly,V_LIST)
-print("AGG Poly:",agg_poly)
-print("Agg at zeta:", poly_evaluate(agg_poly, Evaluation_point_Zeta, S_PRIME))
+
+
+# print("AGG Poly:",agg_poly)
+# print("Agg at zeta:", poly_evaluate(agg_poly, Evaluation_point_zeta, S_PRIME))
 
 phi_z, phi_zw=proof_contents_phi(agg_poly,l_agg,Evaluation_point_Zeta, zeta_omega)
+
 prover_proof=construct_proof(C_b_v,C_acc_ip,C_acc_x,C_acc_y,P_x_zeta,P_y_zeta,s_zeta,b_zeta,acc_ip_zeta,acc_x_zeta, acc_y_zeta,C_q, l_agg_at_zeta_omega, phi_z,phi_zw)
+
 end_is=time.time()
-print(prover_proof)
+print("veriferEnd_proof:",prover_proof)
 print("proof generation time:", end_is- start_is)
-print(phi_z)
-print(phi_zw)
+
+print(normalize(phi_z))
+print(normalize(phi_zw))
+
 
--- jam/ring_vrf/ring_proof/linearization_polynomial.py
+++ jam/ring_vrf/ring_proof/linearization_polynomial.py
@@ -1,14 +1,16 @@
+import sys
 from sympy import symbols, Poly
 from jam.ring_vrf.ring_proof.constants import omega, S_PRIME, D
 from jam.ring_vrf.ring_proof.prover_witness_polynomials import acc_x_I, acc_y_I, acc_ip_I
-from jam.ring_vrf.ring_proof. polynomial_interpolation import poly_add,poly_subtract,poly_multiply,poly_scalar,poly_evaluate
+from jam.ring_vrf.ring_proof. polynomial_vect_ops import poly_add,poly_subtract,poly_multiply,poly_scalar,poly_evaluate
 from jam.ring_vrf.ring_proof.quotient_polynomial import P_x_zeta, P_y_zeta, acc_x_zeta, acc_y_zeta, acc_ip_zeta, s_zeta, \
     b_zeta, Evaluation_point_Zeta
 from jam.ring_vrf.ring_proof.constraints_aggregation import alpha_values
-import sys
+from jam.ring_vrf.ring_proof.KZG_polynomial_commit_open_verify import commit_to_polynomial
+
 sys.set_int_max_str_digits(10000)
 
-scalar_term = (Evaluation_point_Zeta - D[-4] % S_PRIME)
+scalar_term = (Evaluation_point_Zeta- D[-4] % S_PRIME)
 
 # l1 = (zeta - omega^(N - 4)) * acc_ip_I
 l1 = poly_scalar(acc_ip_I, scalar_term,S_PRIME)
@@ -26,6 +28,7 @@ l3= poly_scalar(poly_add(poly_scalar(acc_x_I,(b_zeta*((acc_y_zeta-P_y_zeta)%S_PR
 zeta_omega=(Evaluation_point_Zeta * omega ) % S_PRIME
 
 
+
 def linearization_contraint_aggregated(l1,l2,l3,alphas):
     l_c=[l1,l2,l3]
     l_x=[0]
@@ -35,7 +38,11 @@ def linearization_contraint_aggregated(l1,l2,l3,alphas):
 
 import time
 st_time=time.time()
-l_agg=linearization_contraint_aggregated(l1,l2,l3,alpha_values)
+l_agg=linearization_contraint_aggregated(l1,l2,l3,alpha_values)\
+
+#check
+l_agg_cmt=commit_to_polynomial(l_agg)#
+print("prover_end_cmt:",l_agg_cmt)
 
 l_agg_at_zeta_omega = poly_evaluate(l_agg, zeta_omega, S_PRIME)
 
@@ -46,3 +53,4 @@ print("value of l_agg_zeta_omega:",l_agg_at_zeta_omega)
 # print('tt;',end_time-st_time)
 print("zeta_omega:",zeta_omega)
 
+# res=44920803151022524355293335048980144801879910321560440534834966064612116887943
