1e9fc1034a3c2eb360307cf75544d9e27d4bed45
konasiva7 <sivakona@chainscore.finance>
2025-05-16 12:42:07 +0530

    updated vrf

--- jam/ring_vrf/vrf.py
+++ jam/ring_vrf/vrf.py
@@ -158,7 +158,7 @@ class VRF(ABC):
         proof_to_hash_domain_separator_front = b"\x03"
         proof_to_hash_domain_separator_back = b"\x00"
         beta_string = Hash.sha512(
-            self.curve.SUITE_STRING.encode() + 
+            self.curve.SUITE_STRING+
             proof_to_hash_domain_separator_front + 
             (
                 gamma
