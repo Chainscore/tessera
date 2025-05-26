0b34519b640c75d760b8e9eb70dfb549b84f1509
faizChainscore <faiz@chainscore.finance>
2025-05-26 12:58:54 +0530

    removed dummy data with db data in ce139/140

--- jam/network/protocols/ce_139.py
+++ jam/network/protocols/ce_139.py
@@ -1,3 +1,4 @@
+from jam.config.settings import settings
 from jam.network.protocols.ce_139_base import SegmentShardRequestBase, Response
 from typing import cast
 
@@ -5,7 +6,11 @@ from jam.config.logging import logger
 from jam.network.quic import QuicServerProtocol
 
 from jam.network.protocols.base import PrefixType
-
+from jam.storage.db.kv import KVStore
+from jam.types.work.shard import SegmentsShard
+from jam.work_package.stores.mappings import ErasureShardsMap
+from jam.work_package.stores.segments import SegmentShardsDA
+from jam.types.base.integers.fixed import U16
 from tests.dummy.utils import create_dummy_bytes12
 
 
@@ -31,9 +36,18 @@ class SegmentShardRequest(SegmentShardRequestBase):
     def server_intercept(self, node: Node, buffer: bytes, server: QuicServerProtocol, stream_id: int):
         request = self.parse_request(buffer)
         logger.info("Handling CE139 shard request")
+        d3l = KVStore(settings.D3L_PATH)
+        er_shards_db = ErasureShardsMap(d3l)
+        ss_da = SegmentShardsDA(d3l)
 
-        # Fetch segments...
-        response = Response([create_dummy_bytes12()])
+        # Fetching segments...
+        response = Response()
+        for item in request:
+            ss_key = er_shards_db.get_ss_root(root=item.erasure_root, shard_index=item.shard_Index)
+            seg_shards: SegmentsShard = ss_da.get(root=ss_key.segment_shard_root)[0]
+            for index in item.seg_indexes:
+                response.append(seg_shards[index])
+
         ack = self._prefix.encode() + response.encode()
         server.stream_and_close(stream_id, ack)
 
