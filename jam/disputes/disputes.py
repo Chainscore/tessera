a106a24602ee978d2939686198a5d1b493fa64c1
AkashChainS <akash@chainscore.finance>
2025-05-26 18:54:55 +0530

    fix(disputes): some key validation as per [Graypaper §12.3](https://graypaper.fluffylabs.dev/#/5b732de/12c00112c001?v=0.5.2)

--- jam/disputes/disputes.py
+++ jam/disputes/disputes.py
@@ -88,6 +88,11 @@ class Disputes:
                 fault.key, message_bytes, fault.target, fault.signature
             ):
                 raise DisputesError(DisputesErrorCode.BAD_SIGNATURE)
+            if fault.key not in [v.ed25519
+                for v in (*state.lambda_, *state.kappa)]:
+                raise DisputesError(DisputesErrorCode.BAD_AUDITOR_KEY)
+
+
         # Verifying culprit signatures
         for culprit in disputes.culprits:
             message_bytes = b"jam_guarantee"
@@ -95,6 +100,10 @@ class Disputes:
                 culprit.key, message_bytes, culprit.target, culprit.signature
             ):
                 raise DisputesError(DisputesErrorCode.BAD_SIGNATURE)
+            if culprit.key not in [validator.ed25519
+                for validator in (*state.lambda_, *state.kappa)]:
+                raise DisputesError(DisputesErrorCode.BAD_GUARANTOR_KEY)
+
 
         # Verifying verdicts are sorted by target
         for verdict in disputes.verdicts:
