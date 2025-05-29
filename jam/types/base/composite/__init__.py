57ed2bc1060f648e87b7a9d2d7eb7e844d41ba4c
prasad-kumkar <prasad@chainscore.finance>
2025-05-30 02:41:36 +0530

    feat(typings): added option, added decode() to codable for cleaner decoding

--- jam/types/base/composite/__init__.py
+++ jam/types/base/composite/__init__.py
@@ -1,11 +1,11 @@
-from .choice import Choice, decodable_choice
-from .option import Option, decodable_option
-
-__all__ = [
-    # Choice types
-    "Choice",
-    "Option",
-    # Decodable types
-    "decodable_choice",
-    "decodable_option",
-]
+# from .choice import Choice, decodable_choice
+# from .option import Option, decodable_option
+#
+# __all__ = [
+#     # Choice types
+#     "Choice",
+#     "Option",
+#     # Decodable types
+#     "decodable_choice",
+#     "decodable_option",
+# ]
