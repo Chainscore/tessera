560fb9df97cccb061dbb5dbe9e0eb8d343a3cd38
konasiva7 <sivakona@chainscore.finance>
2025-06-10 18:43:08 +0530

    modified l2 l3 computation

--- jam/ring_vrf/ring_proof/proof/linearization_poly.py
+++ jam/ring_vrf/ring_proof/proof/linearization_poly.py
@@ -1,7 +1,9 @@
 import sys
+
+from jam.ring_vrf.curve.specs.bandersnatch import BandersnatchParams
 from jam.ring_vrf.ring_proof.constants import OMEGA as omega, D_512 as D, S_PRIME
 from jam.ring_vrf.ring_proof.polynomial.ops import (
-    poly_add, poly_scalar
+    poly_add, poly_scalar, vect_mul, poly_multiply
 )
 from jam.ring_vrf.ring_proof.polynomial.ops import poly_evaluate
 from jam.ring_vrf.ring_proof.transcript.phases import phase2_eval_point
@@ -9,12 +11,13 @@ from jam.ring_vrf.ring_proof.transcript.phases import phase2_eval_point
 class LAggPoly:
 
     def __init__(self, cur_t, C_q, fixed_cols, witness_res, alphas):
-        self.t, self.zeta= phase2_eval_point(cur_t,C_q)
+        self.t, self.zeta = phase2_eval_point(cur_t, C_q)
         self.zeta_omega = (self.zeta * omega) % S_PRIME
         self.scalar_term = (self.zeta - D[-4]) % S_PRIME
         self.fs= fixed_cols
         self.wts= witness_res
         self.alphas= alphas
+        self.a= BandersnatchParams.EDWARDS_A
 
     def evaluate_polys_at_zeta(self):
         self.P_x_zeta = poly_evaluate(self.fs[0].coeffs, self.zeta, S_PRIME)
@@ -29,17 +32,48 @@ class LAggPoly:
         return poly_scalar(self.wts[3].coeffs, self.scalar_term, S_PRIME)
 
     def compute_l2(self):
-        inner = (self.b_zeta * pow((self.acc_x_zeta - self.P_x_zeta) % S_PRIME, 2, S_PRIME)) % S_PRIME
-        left = poly_scalar(self.wts[1].coeffs, inner, S_PRIME)
-        right = poly_scalar(self.wts[2].coeffs, (1 - self.b_zeta) % S_PRIME, S_PRIME)
-        return poly_scalar(poly_add(left, right, S_PRIME), self.scalar_term, S_PRIME)
+
+        x1, y1= self.acc_x_zeta, self. acc_y_zeta
+        x2, y2= self.P_x_zeta, self.P_y_zeta
+        b= self.b_zeta
+        coeff_a= self.a
+
+        C_acc_x= (b *(y1*y2 + (coeff_a * x1 *x2)) % S_PRIME + (1-b) %S_PRIME ) % S_PRIME
+        C_acc_y=0
+        C_acc_x_f= C_acc_x * self.scalar_term
+        C_acc_y_f = C_acc_y* self.scalar_term
+
+        term1= vect_mul( self.wts[1].coeffs, C_acc_x_f, S_PRIME)
+        term2= vect_mul(self.wts[2].coeffs,C_acc_y_f, S_PRIME)
+        res=poly_add(term1, term2, S_PRIME)
+        return res
+
+        # inner = (self.b_zeta * pow((self.acc_x_zeta - self.P_x_zeta) % S_PRIME, 2, S_PRIME)) % S_PRIME
+        # left = poly_scalar(self.wts[1].coeffs, inner, S_PRIME)
+        # right = poly_scalar(self.wts[2].coeffs, (1 - self.b_zeta) % S_PRIME, S_PRIME)
+        # return poly_scalar(poly_add(left, right, S_PRIME), self.scalar_term, S_PRIME)
+
 
     def compute_l3(self):
-        term1_scalar = (self.b_zeta * ((self.acc_y_zeta - self.P_y_zeta) % S_PRIME) + (1 - self.b_zeta)) % S_PRIME
-        term2_scalar = (self.b_zeta * ((self.acc_x_zeta - self.P_x_zeta) % S_PRIME)) % S_PRIME
-        term1 = poly_scalar(self.wts[1].coeffs, term1_scalar, S_PRIME)
-        term2 = poly_scalar(self.wts[2].coeffs, term2_scalar, S_PRIME)
-        return poly_scalar(poly_add(term1, term2, S_PRIME), self.scalar_term, S_PRIME)
+
+        b= self.b_zeta
+        x1, y1= self.acc_x_zeta, self.acc_y_zeta
+        x2, y2= self.P_x_zeta, self.P_y_zeta
+        C_acc_x=0
+        C_acc_y=((b*(x1*y2 - x2*y1)) %S_PRIME+(1- b) %S_PRIME) %S_PRIME
+        C_acc_x *=self.scalar_term
+        C_acc_y*=self.scalar_term
+
+        term1= poly_scalar(self.wts[1].coeffs , C_acc_x, S_PRIME)
+        term2=poly_scalar(self.wts[2].coeffs, C_acc_y, S_PRIME)
+        res= poly_add(term1, term2, S_PRIME)
+        return res
+
+        # term1_scalar = (self.b_zeta * ((self.acc_y_zeta - self.P_y_zeta) % S_PRIME) + (1 - self.b_zeta)) % S_PRIME
+        # term2_scalar = (self.b_zeta * ((self.acc_x_zeta - self.P_x_zeta) % S_PRIME)) % S_PRIME
+        # term1 = poly_scalar(self.wts[1].coeffs, term1_scalar, S_PRIME)
+        # term2 = poly_scalar(self.wts[2].coeffs, term2_scalar, S_PRIME)
+        # return poly_scalar(poly_add(term1, term2, S_PRIME), self.scalar_term, S_PRIME)
 
     def linearize(self, l1, l2, l3):
         l_agg = [0]
@@ -48,18 +82,14 @@ class LAggPoly:
         return l_agg
 
     def l_agg_poly(self):
-        self.evaluate_polys_at_zeta()  # fills P_x_zeta … acc_y_zeta
 
+        self.evaluate_polys_at_zeta()  # fills P_x_zeta … acc_y_zeta
         l1 = self.compute_l1()
         l2 = self.compute_l2()
         l3 = self.compute_l3()
         l_agg = self.linearize(l1, l2, l3)
-        # print("l_agg;", l_agg)
 
         l_agg_zeta_omega = poly_evaluate(l_agg, self.zeta_omega, S_PRIME)
-
-        # return the bundle you want to print later
-        # print("Zeta:", self.zeta)
 
         return self.t, self.zeta, {
             # "Zeta":self.zeta,
