0ea79d58874eed4ace5753344747a857e43e6b22
AkashChainS <akash@chainscore.finance>
2025-06-23 19:08:51 +0530

    modified audit file for a_n, s_n for each validators

--- jam/audit/audit.py
+++ jam/audit/audit.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from typing import List, Optional, Set, Tuple
 from tsrkit_types import Null, Bytes, Bytes64
-from jam.types.protocol.core import CoreIndex
+from jam.types.protocol.core import CoreIndex, ValidatorIndex
 from jam.ring_vrf.curve.specs.bandersnatch import BandersnatchPoint, Bandersnatch_TE_Curve
 from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey
 from jam.types.work.report import WorkReport
@@ -18,16 +18,20 @@ from jam.audit.utils import Ξ,F,power_set
 from jam.ring_vrf.ietf.ietf import IETF_VRF
 from jam.types.work.package import WorkPackage
 from jam.work_package.processor import Processor
+from tsrkit_types.sequences import TypedVector
+from tsrkit_types.integers import Uint
+
+
 
 @dataclass
 class AuditingAndJudgement:
 
     def __init__(self):
         self.vrf = VRF
-        self.assurance = Assurances.transition(state=state, block=Block)
+        self.state,self.assurance = Assurances.transition(state=state, block=Block)
         self.process = Processor
 
-    def report_to_be_audit(self, state: state) -> List[WorkReport]:
+    def report_to_be_audit(self) -> List[Optional[WorkReport]]:
         """
         Function Q define in Eq. 17.1 and 17.2
         This function define the sequence of work_report which required to audit(Q)
@@ -39,24 +43,24 @@ class AuditingAndJudgement:
             state : Pending Work Reports
 
         Returns:
-            Array of Work Reports to be Audit
+            List of Optional[WorkReport] .
         """
 
-        pre_auditing_report = []
+        pre_auditing_report: List[Optional[WorkReport]] = []
 
-        for i, (report, slot) in enumerate(state.rho):
+        for i, (report, _slot) in enumerate(self.state.rho):
             if report in self.assurance:
                 pre_auditing_report.append(report)
             else:
-                pre_auditing_report.append(Null)
+                pre_auditing_report.append(None)
 
         return pre_auditing_report
 
-    @staticmethod
-    def vrf_signature_bandersnatch(header: Header, bandersnatch_key: BandersnatchPublic, tranche_index: int = None , w_report: Optional[WorkReport] = None) -> Bytes[96]:
-        # bandersnatch_key : BandersnatchPublic  [replace this with] => state.kappa[0]["bandersnatch"]
-
+    def vrf_signature_bandersnatch(self, header: Header, validator_index: ValidatorIndex, tranche_index: int = 0, w_report: Optional[WorkReport] = None) -> Bytes[96]:
         """
+        refer 17.3
+        https://graypaper.fluffylabs.dev/#/cc517d7/1e89001e8f00?v=0.6.5
+
         s_0: The initial VRF (Verifiable Random Function) signature used to select work-reports for auditing in the first tranche (n=0).
 
         Sources:
@@ -64,7 +68,7 @@ class AuditingAndJudgement:
 
         Args:
             header : Header for fetching its entropy (H_v)
-            bandersnatch_key :
+            validator_index :
             tranche_index :
             w_report : Work Report
 
@@ -81,7 +85,7 @@ class AuditingAndJudgement:
 
         random_quantity = bytes(SIGNING_CONTEXTS["audit"]) + entropy_vrf_proof #Xv + y(Hv)
 
-        key = int.from_bytes(bytes.fromhex(str(bandersnatch_key)))
+        key = int.from_bytes(bytes.fromhex(str(self.state.kappa[validator_index]["bandersnatch"])))
 
         output_point, proof=  vrf.prove(alpha=b"" , secret_key=key,  additional_data=random_quantity,salt=b"")
         op_bt_str= output_point.point_to_string()
@@ -89,7 +93,7 @@ class AuditingAndJudgement:
         proof_bt_str= proof[0].to_bytes()+ proof[1].to_bytes()
         signature= op_bt_str + proof_bt_str #Expected S0 (96bytes)
 
-        if tranche_index is not None and tranche_index > 0 and w_report is not None:
+        if tranche_index > 0 and w_report is not None:
             random_quantity+= bytes(Hash.blake2b(w_report.encode()).encode()) + bytes(tranche_index) # refer 17.15
 
         # F Function needs to be implemented Here expected to return [sets of signatures]
@@ -97,7 +101,7 @@ class AuditingAndJudgement:
         return signature
 
     # Verifiable Random Selection Function (within 10 cores) a_n
-    def vrs_func(self, header: Header, state: state) -> List[Tuple[CoreIndex, WorkReport]]:
+    def vrs_func(self, header: Header, validator_index:ValidatorIndex) -> List[Tuple[CoreIndex, WorkReport]]:
 
         """
         This function give the non-empty-item to audit through a verifiable random selection of ten cores:
@@ -108,22 +112,29 @@ class AuditingAndJudgement:
         Equations:
             17.7 r = y(So)
             17.6 p = f([(c, Qc) | c <- Nc ], r )
-            17.5 ao = {(c, w) | (c, w) E p... + 10, w != Phi }
+            17.5 ao = {(c, w) | (c, w) E p... + 10, w != None }
         """
 
         # Equation : 17.7 (r = y(So))
         vrf = IETF_VRF(Bandersnatch_TE_Curve, BandersnatchPoint)
-        entropy = vrf.proof_to_hash(BandersnatchPoint.encode_to_curve(AuditingAndJudgement.vrf_signature_bandersnatch(header=header, bandersnatch_key=state.kappa[0]["bandersnatch"])))[:32]
+        entropy = vrf.proof_to_hash(BandersnatchPoint.encode_to_curve(self.vrf_signature_bandersnatch(header=header, validator_index=validator_index)))[:32]
 
-        # Equation :  17.6 p = f([(c, Qc) | c <- Nc ], r )
-        audit_report = self.report_to_be_audit(state)
-        core_report =  [(c, w_r) for c, w_r in enumerate(audit_report)]
+         # Equation 17.6: p = f([(c, Qc) | c <- Nc ], r)
+        audit_report = self.report_to_be_audit()
+        core_indexes=TypedVector[Uint[32]]([])
+        # indexes of c existing in the audit_report for shuffling on it
+        for i, w_r in enumerate(audit_report):
+            core_indexes.append(Uint[32](i))
+        # Shuffle based on entropy
+        shuffle_report = shuffle(entropy, core_indexes)
 
-        shuffle_report = shuffle(entropy, core_report)
-
-        # Eq. 17.5 : ao = {(c, w) | (c, w) E p... + 10, w != Phi }tes64(signature))
-        vrs_list = [(c, w) for (c, w) in shuffle_report if w is not Null]
-
+        # Eq. 17.5 : ao = {(c, w) | (c, w) E p... + 10, w != Null }tes64(signature))
+        vrs_list:List[Tuple[CoreIndex, WorkReport]] = []
+        for i in shuffle_report:
+            if audit_report[i] is not None:
+                vrs_list.append((CoreIndex(i), audit_report[i]))
+            if len(vrs_list) == 10:
+                break
         return vrs_list
 
     @staticmethod
@@ -212,5 +223,3 @@ class AuditingAndJudgement:
             # Explicitely modifying to the ByteArray64 format
             judgement_set.append(Bytes64(signature))
         return judgement_set
-
-
