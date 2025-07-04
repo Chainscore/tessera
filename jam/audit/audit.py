ff876e13843c2fcc4ff211bcc026363da0dbf24c
AkashChainS <akash@chainscore.finance>
2025-07-04 15:16:00 +0530

    tranch to be reformated

--- jam/audit/audit.py
+++ jam/audit/audit.py
@@ -1,11 +1,13 @@
-from dataclasses import dataclass
-from typing import List, Optional, Tuple
+from dataclasses import dataclass, field
+from typing import Dict, List, Optional, Set, Tuple
 from tsrkit_types import Null, Bytes, TypedVector, U32, Bytes, Uint, Option
+from jam.audit.utils import bandersnatch_f
+from jam.network.protocols.ce_136 import OptRep
 from jam.types.protocol.core import CoreIndex, TimeSlot, ValidatorIndex
 from jam.ring_vrf.curve.specs.bandersnatch import BandersnatchPoint, Bandersnatch_TE_Curve
 from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey
 from jam.types.work.report import WorkReport
-from jam.state.state import state
+from jam.state.state import State, state
 from jam.types.block.header import Header
 from jam.utils.constants import CORE_COUNT, CURRENT_TIME, SLOT_PERIOD, AUDIT_PERIOD, SIGNING_CONTEXTS
 from jam.ring_vrf.vrf import VRF
@@ -24,13 +26,21 @@ from tsrkit_types.integers import Uint
 
 @dataclass
 class AuditingAndJudgement:
+    state: State
+    assurance: TypedVector[OptRep]
+    tranche_index: int
+    block: Header
+    # In-memory ephemeral storage during the auditing process
+    an_map: Dict[WorkReportHash, Set[ValidatorIndex]] = field(default_factory=dict)
+    jt_map: Dict[WorkReportHash, Set[ValidatorIndex]] = field(default_factory=dict)
+    jf_map: Dict[WorkReportHash, Set[ValidatorIndex]] = field(default_factory=dict)
+    audited_wrs: Set[WorkReportHash] = field(default_factory=set)
+    # def __init__(self, current_state, current_assurances):
+    #     self.state = current_state
+    #     self.assurance = current_assurances
+    #     # self.process = Processor
 
-    def __init__(self, current_state, current_assurances):
-        self.state = current_state
-        self.assurance = current_assurances
-        # self.process = Processor
-
-    def report_to_be_audit(self,available_reports : TypedVector[OptionHash]) -> TypedVector[OptionHash]:
+    def report_to_be_audit(self,available_reports : TypedVector[OptRep]) -> TypedVector[OptRep]:
         """
         Function Q define in Eq. 17.1 and 17.2
         This function define the sequence of work_report which required to audit(Q)
@@ -45,8 +55,8 @@ class AuditingAndJudgement:
             Array of Work Reports to be Audit
         """
 
-        pre_auditing_report =  TypedVector[OptionHash]([])
-        for i, (report, slot) in enumerate(state.rho):
+        pre_auditing_report =  TypedVector[OptRep]([])
+        for i, (report, slot) in enumerate(self.state.rho):
             if report in available_reports:
                 pre_auditing_report.append(report)
             else:
@@ -55,7 +65,7 @@ class AuditingAndJudgement:
         return pre_auditing_report
 
     @staticmethod
-    def vrf_signature_bandersnatch(entropy_source: BandersnatchVrfSignature, bandersnatch_key: BandersnatchPublic, tranche_index: int = 0, w_report: Optional[WorkReport] = None) -> Bytes[96]:
+    def vrf_signature_bandersnatch(entropy_source: BandersnatchVrfSignature, bandersnatch_key: BandersnatchPublic, tranche_index: int = 0, work_report: Optional[WorkReport] = None) -> Bytes[96]:
         """
         s_0: The initial VRF (Verifiable Random Function) signature used to select work-reports for auditing in the first tranche (n=0).
 
@@ -70,6 +80,8 @@ class AuditingAndJudgement:
 
         Returns:
              randomness for entropy
+
+        refer 17.3 && 17.15
         """
 
         vrf = IETF_VRF(Bandersnatch_TE_Curve, BandersnatchPoint)
@@ -86,17 +98,22 @@ class AuditingAndJudgement:
         proof_bt_str = proof[0].to_bytes(32, 'little') + proof[1].to_bytes(32, 'little')
         signature = op_bt_str + proof_bt_str  # Expected S0 (96bytes)
 
-        if tranche_index is not Null and tranche_index > 0 and w_report is not Null:
-            random_quantity += Bytes(Hash.blake2b(w_report.encode()).encode()) + Bytes(tranche_index)  # refer 17.15
+        if tranche_index is not Null and tranche_index > 0 and work_report is not None:
+            random_quantity += Bytes(Hash.blake2b(work_report.encode()).encode()) + Bytes(tranche_index)  # refer 17.15
 
         return signature
 
 
-    def vrs_func(self, entropy_source: BandersnatchVrfSignature, bandersnatch_key: BandersnatchPublic) -> List[Tuple[CoreIndex, WorkReportHash]]:
+    def vrs_func(self, entropy_source: BandersnatchVrfSignature, bandersnatch_key: BandersnatchPublic,tranch_index:int,work_report:Optional[WorkReport]=None) -> List[Tuple[CoreIndex, WorkReportHash]]:
+        """
+        This is basically a_n assigns certain wr_list to the validator whose bandersnatck key we are passing as args
+        s_n(Hv,validator_bandersnatch,tranche_index,work_report)
 
+        refer 17.16
+        """
         # Equation : 17.7 (r = y(So))
         vrf = IETF_VRF(Bandersnatch_TE_Curve, BandersnatchPoint)
-        entropy = vrf.proof_to_hash(BandersnatchPoint.encode_to_curve(self.vrf_signature_bandersnatch(entropy_source=entropy_source, bandersnatch_key=bandersnatch_key)))[:32]
+        entropy = vrf.proof_to_hash(BandersnatchPoint.encode_to_curve(self.vrf_signature_bandersnatch(entropy_source=entropy_source, bandersnatch_key=bandersnatch_key,tranche_index=tranch_index,work_report=work_report)))[:32]
 
         #  Equation :  17.6 p = f([(c, Qc) | c <- Nc ], r )
         audit_report = self.report_to_be_audit(self.assurance)
@@ -140,7 +157,7 @@ class AuditingAndJudgement:
         return tranche_index
 
 
-    def validator_announcement_statement(self, header: Header, validator_index: ValidatorIndex) -> set[Bytes[64]]:
+    def validator_announcement_statement(self, header: Header, validator_index: ValidatorIndex) -> bytes:
         """
         Validator’s announcement statements must be in the set S
         refer to Eq. 17.9
@@ -156,25 +173,18 @@ class AuditingAndJudgement:
         header_hash = bytes(Hash.blake2b(header.encode()))
 
         # x_n  => Serializing the vrs_list -> x_n
-        vrs_list=self.vrs_func(header.entropy_source,self.state.kappa[validator_index].bandersnatch)
+        vrs_list=self.vrs_func(header.entropy_source,self.state.kappa[validator_index].bandersnatch,tranches_index)
         vrs_bytes=bytes()
         for (core,wr) in vrs_list:
             vrs_bytes += core.encode() + Hash.blake2b(wr.encode()).encode()
 
         #loop through the validators and signing the message with their ed25519publickey
-        # S
-        announcement_statement = signing_context + bytes(tranches_index) + vrs_bytes +  header_hash
+        announcement_context= signing_context + bytes(tranches_index) + vrs_bytes +  header_hash
 
-        statement_set : set[Bytes[64]] = set()
+        statement_signature : bytes= bandersnatch_f(key=self.state.kappa[validator_index].ed25519,context=announcement_context)
 
-        for validator in state.kappa:
-            private_key = Ed25519PrivateKey.from_private_bytes(bytes(validator.ed25519))
-            # Sign will give out a 64Byte Signature
-            signature = private_key.sign(announcement_statement)
-            # Explicitly modifying to the ByteArray64 format
-            statement_set.add(signature)
 
-        return statement_set
+        return statement_signature
     #
     # def evaluate_core_mappings (self, core_index: int , package: WorkPackage, ):
     #     # refer 17.17
@@ -182,41 +192,26 @@ class AuditingAndJudgement:
     #         return self.process.process_bundle(core=core_index, bundle=package, sr_lookup=)
     #     else:
     #         return False
-    #
-    # def validator_judment_mapping(self,work_report:WorkReport,state:state)-> List[Bytes[64]]:
-    #     """
-    #      Go through the evaluate_core_mappings Func (refer 17.17) and provide its validity where the wr is valid or not
-    #      Return :
-    #         Set of Judgment signatures from each (kappa/current) validator
-    #     refer equ 17.18
-    #     """
-    #     judgement_set=[]
-    #     for validator in state.kappa:
-    #         private_key = Ed25519PrivateKey.from_private_bytes(bytes(validator.ed25519))
-    #         signing_context = SIGNING_CONTEXTS["valid"] if evaluate_core_mappings(work_report) else SIGNING_CONTEXTS["invalid"]
-    #         message= signing_context + Hash.blake2b(work_report.encode())
-    #         # Sign will give out a 64Byte Signature
-    #         signature = private_key.sign(message)
-    #         # Explicitely modifying to the ByteArray64 format
-    #         judgement_set.append(Bytes[64](signature))
-    #     return judgement_set
 
-    def validator_judment_mapping(self,work_report:WorkReport)-> List[Bytes[64]]:
+
+    def validator_judment_mapping(self,header:Header,validator_index:ValidatorIndex,isTrue:Optional[bool]=None,tranche_index:int=0)-> list[bytes] :
         """
          Go through the evaluate_core_mappings Func (refer 17.17) and provide its validity where the wr is valid or not
          Return :
             Set of Judgment signatures from each (kappa/current) validator
         refer equ 17.18
         """
-        judgement_set=[]
-        for validator in self.state.kappa:
-            # private_key = Ed25519PrivateKey.from_private_bytes(bytes(validator.ed25519))
-            # signing_context = SIGNING_CONTEXTS["valid"] if evaluate_core_mappings(work_report) else SIGNING_CONTEXTS["invalid"]
+        # judgement_map={True:[],False:[]} # later on might change to list of judgement (Judgement Signatures)
+        judgment_list=[]
+        private_key = Ed25519PrivateKey.from_private_bytes(bytes(self.state.kappa[validator_index].ed25519))
+        a_n=self.vrs_func(header.entropy_source,self.state.kappa[validator_index].bandersnatch,tranche_index)
+        for core_index,wr_hash in enumerate(a_n):
+
+            # signing_context = SIGNING_CONTEXTS["valid"] if evaluate_core_mappings(wr_hash) else SIGNING_CONTEXTS["invalid"]
             signing_context = SIGNING_CONTEXTS["valid"] if True else SIGNING_CONTEXTS["invalid"]
 
-            message= signing_context + Hash.blake2b(work_report.encode())
-            # Sign will give out a 64Byte Signature
-            signature = private_key.sign(message)
-            # Explicitely modifying to the ByteArray64 format
-            judgement_set.append(Bytes64(signature))
-        return judgement_set
+            message= signing_context + )
+        signature = private_key.sign(message)
+                # Explicitely modifying to the ByteArddddray64 format
+        judgment_list.append(bytes(signature))
+        return judgment_list
--- tests/unit/audit/test_tranche.py
+++ tests/unit/audit/test_tranche.py
@@ -11,6 +11,7 @@ from jam.types import state
 from jam.types.protocol.core import CoreIndex
 from jam.types.protocol.crypto import Hash
 from jam.types.work.report import WorkReport
+from jam.utils.constants import VALIDATOR_COUNT
 
 def populate_db(db, wr_list, tranche):
     """
@@ -55,75 +56,85 @@ def dummy_vrf_check(wr, tranche):
     # vrf=utils.bandersnatch_y(bytes)
     return random.random() < 0.3
 
-# if __name__ == "__main__":
-#     with tempfile.TemporaryDirectory() as tmpdir:
-#         jam.settings.setup_setting(data_path=tmpdir, seed=0)
-#         db = jam.settings.settings.main_db
+def audit_condition(db,work_report:WorkReport,tranche_index:int):
+    """
+    refer 17.19
+    """
 
-#         all_wrs = ['wr1', 'wr2', 'wr3', 'wr4', 'wr5']
-#         audited_wrs = set()
-#         tranche = 0
-#         max_tranches = 10
-
-#         while tranche < max_tranches:
-#             print(f"\n=== Tranche {tranche} ===")
-#             wrs_to_audit = [wr for wr in all_wrs if wr not in audited_wrs]
-#             if not wrs_to_audit:
-#                 print("✅ All WRs audited successfully.")
-#                 break
-
-#             populate_db(db, wrs_to_audit, tranche)
-
-#             for wr in wrs_to_audit:
-#                 announced = An(db, wr, tranche)
-#                 true_judged = J_t(db, wr, tranche)
-#                 false_judged = J_f(db, wr, tranche)
-
-#                 print(f"\nWR: {wr}")
-#                 print(f"  Announced by: {announced}")
-#                 print(f"  True Judgements: {true_judged}")
-#                 print(f"  False Judgements: {false_judged}")
-
-#                 # Auditing condition:
-#                 # - No false judgements
-#                 # - OR dummy_vrf_check returns True
-#                 if len(false_judged) == 0 or dummy_vrf_check(wr, tranche):
-#                     audited_wrs.add(wr)
-#                     print(f"✅ WR {wr} audited in tranche {tranche}.")
-#                 else:
-#                     print(f"❌ WR {wr} NOT audited in tranche {tranche}, will retry in next tranche.")
-
-#             tranche += 1
-#             time.sleep(2)  # Simulate time between tranches
-
-#         if len(audited_wrs) == len(all_wrs):
-#             print("\n🎉 All WRs successfully audited across all tranches!")
-#         else:
-#             failed_wrs = [wr for wr in all_wrs if wr not in audited_wrs]
-#             print("\n⚠️ Audit did not complete for the following WRs:")
-#             for wr in failed_wrs:
-#                 print(f"  - {wr}")
-
-
-def dummy_wrs(core_range:int)->list[WorkReport]:
-    """n: No. of dummy wrs till the no. of cores required"""
-    wrs=[]
-    for core_index in range(core_range):
-        dummy_wr=WorkReport.empty()
-        dummy_wr.core_index=CoreIndex(core_index)
-        wrs.append(dummy_wr)
-    return wrs
+    if (len(J_f(db,wr=work_report,tranch=tranche_index)) and len(An(db,work_report,tranche=tranche_index)) == len(J_t(db,work_report,tranche=tranche_index))) || len(J_t(db,work_report,tranche=tranche_index))== 2/3*VALIDATOR_COUNT):
+        return True
+    else:
+        return False
 
 if __name__ == "__main__":
     with tempfile.TemporaryDirectory() as tmpdir:
         jam.settings.setup_setting(data_path=tmpdir, seed=0)
         db = jam.settings.settings.main_db
-        setup_state(db, GhostState.genesis())
-        from jam.state.state import state as updated_state
-        rho=updated_state.rho
-        dummy_wr_list=dummy_wrs(2)
-        print(dummy_wr_list)
 
-        # audited_wrs = set()
-        # tranche = 0
-        # max_tranches = 10
+        all_wrs = ['wr1', 'wr2', 'wr3', 'wr4', 'wr5']
+        audited_wrs = set()
+        tranche = 0
+        max_tranches = 10
+
+        while tranche < max_tranches:
+            print(f"\n=== Tranche {tranche} ===")
+            wrs_to_audit = [wr for wr in all_wrs if wr not in audited_wrs]
+            if not wrs_to_audit:
+                print("✅ All WRs audited successfully.")
+                break
+
+            populate_db(db, wrs_to_audit, tranche)
+
+            for wr in wrs_to_audit:
+                announced = An(db, wr, tranche)
+                true_judged = J_t(db, wr, tranche)
+                false_judged = J_f(db, wr, tranche)
+
+                print(f"\nWR: {wr}")
+                print(f"  Announced by: {announced}")
+                print(f"  True Judgements: {true_judged}")
+                print(f"  False Judgements: {false_judged}")
+
+                # Auditing condition:
+                # - No false judgements
+                # - OR dummy_vrf_check returns True
+                if len(false_judged) == 0 or dummy_vrf_check(wr, tranche):
+                    audited_wrs.add(wr)
+                    print(f"✅ WR {wr} audited in tranche {tranche}.")
+                else:
+                    print(f"❌ WR {wr} NOT audited in tranche {tranche}, will retry in next tranche.")
+
+            tranche += 1
+            time.sleep(2)  # Simulate time between tranches
+
+        if len(audited_wrs) == len(all_wrs):
+            print("\n🎉 All WRs successfully audited across all tranches!")
+        else:
+            failed_wrs = [wr for wr in all_wrs if wr not in audited_wrs]
+            print("\n⚠️ Audit did not complete for the following WRs:")
+            for wr in failed_wrs:
+                print(f"  - {wr}")
+
+
+# def dummy_wrs(core_range:int)->list[WorkReport]:
+#     """n: No. of dummy wrs till the no. of cores required"""
+#     wrs=[]
+#     for core_index in range(core_range):
+#         dummy_wr=WorkReport.empty()
+#         dummy_wr.core_index=CoreIndex(core_index)
+#         wrs.append(dummy_wr)
+#     return wrs
+
+# if __name__ == "__main__":
+#     with tempfile.TemporaryDirectory() as tmpdir:
+#         jam.settings.setup_setting(data_path=tmpdir, seed=0)
+#         db = jam.settings.settings.main_db
+#         setup_state(db, GhostState.genesis())
+#         from jam.state.state import state as updated_state
+#         rho=updated_state.rho
+#         dummy_wr_list=dummy_wrs(2)
+#         print(dummy_wr_list)
+
+#         # audited_wrs = set()
+#         # tranche = 0
+#         # max_tranches = 10
