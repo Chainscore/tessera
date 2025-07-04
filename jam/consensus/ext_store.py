d35d44b1a73852ad7b19fec698488d9d363baa59
prasadkumkar <prasad@chainscore.finance>
2025-07-04 14:13:03 +0530

    feat: ext handling

--- jam/consensus/ext_store.py
+++ jam/consensus/ext_store.py
@@ -6,10 +6,10 @@ from tsrkit_types import TypedVector
 
 from jam.preimages import preimages
 from jam.types.block.block import Block
-from jam.types.block.extrinsics.assurances import AssurancesExtrinsic
+from jam.types.block.extrinsics.assurances import AssurancesExtrinsic, AvailAssurance
 from jam.types.block.extrinsics.disputes import Culprits, DisputesExtrinsic, Faults, Verdicts
 from jam.types.block.extrinsics.guarantees import GuaranteesExtrinsic, ReportGuarantee
-from jam.types.block.extrinsics.preimages import PreimagesExtrinsic
+from jam.types.block.extrinsics.preimages import Preimage, PreimagesExtrinsic
 from jam.types.block.extrinsics.tickets import TicketEnvelope
 from jam.types.protocol.core import TimeSlot
 
@@ -36,8 +36,10 @@ class ExtrinsicStore:
         )
     
     # ----- EG ----- #
-    def process_guarantee(self, report_g: ReportGuarantee):
-        """Process an incoming report gurantee"""
+    def import_rg(self, report_g: ReportGuarantee):
+        """
+        Process an incoming report gurantee
+        """
         # Find if it already exists in eg, if so, update the signatures(add new ones); ignore old ones 
         index = -1
         for i, g in enumerate(self.eg):
@@ -48,10 +50,13 @@ class ExtrinsicStore:
             # TODO: Validate guarantees + WR 
             self.eg.append(report_g)
         else:
-            logger.error("Duplicate Work Report found", report=report_g)
+            logger.error("Duplicate Work Report found", report=report_g.to_json())
 
-    def remove_guarantee(self, report_g: ReportGuarantee):
-        """Remove an imported gurantee"""
+    def rm_rg(self, report_g: ReportGuarantee):
+        """
+        Remove an imported report gurantee
+        Logs a warning if we had not collected the extrinsic. This implies our netwokring didn't fn properly.
+        """
         index = -1
         for i, g in enumerate(self.eg):
             if g.report == report_g.report:
@@ -61,23 +66,76 @@ class ExtrinsicStore:
 
        
     # ----- EA ----- #
+    def import_assr(self, assr: AvailAssurance):
+        """
+        Process an incoming assurance 
+        """
+        if assr in self.ea:
+            logger.error("Duplicate Assurance found", assurance=assr.to_json())
+            return 
+        self.ea.append(assr)
+    
+    def rm_assr(self, assr: AvailAssurance):
+        try:
+            indx = self.ea.index(assr)
+            self.ea.pop(indx)
+        except ValueError as e:
+            logger.warning("Assurance was not collected", error=e, assurance=assr.to_json())
+            return 
+    
     # ----- ET ----- #
-    # ----- ED ----- #
+    def import_tkt(self, tkt: TicketEnvelope):
+        """
+        Process an incoming ticket  
+        """
+        if tkt in self.et:
+            logger.error("Duplicate Ticket found", ticket=tkt.to_json())
+            return 
+        self.et.append(tkt)
+    
+    def rm_tkt(self, tkt: TicketEnvelope):
+        try:
+            indx = self.et.index(tkt)
+            self.et.pop(indx)
+        except ValueError as e:
+            logger.warning("Ticket was not collected", error=e, ticket=tkt.to_json())
+            return 
+    
+    # TODO: ----- ED ----- #
+
+
     # ----- EP ----- #
+    def import_pimg(self, pimg: Preimage):
+        """
+        Process an incoming preimage 
+        """
+        if pimg in self.ep:
+            logger.error("Duplicate Preimage found", ticket=tkt.to_json())
+            return 
+        self.ep.append(pimg)
+    
+    def rm_pimg(self, pimg: Preimage):
+        try:
+            indx = self.ep.index(pimg)
+            self.ep.pop(indx)
+        except ValueError as e:
+            logger.warning("Preimage was not collected", error=e, ticket=tkt.to_json())
+            return 
+    
 
     def clear_on_import(self, block: Block):
         # Remove Preimages 
         for preimage in block.extrinsic.preimages:
-            if preimage in self.ep: self.ep.remove(preimage)
+            self.rm_pimg(preimage)
         # Remove Tickets
         for ticket in block.extrinsic.tickets:
-            if ticket in self.et: self.et.remove(ticket)
+            self.rm_tkt(ticket)
         # Remove Assurances 
         for assurance in block.extrinsic.assurances:
-            if assurance in self.ea: self.ea.remove(assurance)
+            self.rm_assr(assurance)
         # Remove WRs
         for grte in block.extrinsic.guarantees:
-            if grte in self.eg: self.eg.remove(grte)
+            self.rm_rg(grte)
         # TODO: Remove disputes 
 
         return
