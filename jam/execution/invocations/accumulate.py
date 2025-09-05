75d69dceb52f32e76c2a3185e8138f319f44a4e5
Prasad Kumkar <prasad@chainscore.finance>
2025-09-05 18:08:27 +0530

    fix: host call fixes, assign ordering, dict sorting

--- jam/execution/invocations/accumulate.py
+++ jam/execution/invocations/accumulate.py
@@ -24,17 +24,16 @@ from jam.utils.constants import MAX_SERVICE_CODE_SIZE
 
 
 class PsiA(InvocationProtocol):
-    def __init__(self, u: GhostPartial, t: TimeSlot, s: ServiceId, g: Gas, o: OperandTuples):
+    def __init__(self, u: GhostPartial, t: TimeSlot, s: ServiceId, g: Gas, o: OperandTuples, entropy: OpaqueHash):
         self.partial_state = u
         self.timeslot = t
         self.service_id = s
         self.gas = g
         self.operandTuples = o
+        self.entropy = entropy
         self.context = AccumulationContext(x=self.initializer_fn(s, u.clone()), y=self.initializer_fn(s, u.clone()))
 
     def table(self):
-        from jam.state.state import state
-
         xs = self.context.x.s_index
         delta = self.context.x.partial_state.service_accounts
         return {
@@ -43,8 +42,7 @@ class PsiA(InvocationProtocol):
                 GeneralFunctions,
                 {  
                     "package": None,
-                    # TODO: This should be posterier
-                    "entropy": state.eta[0],
+                    "entropy": self.entropy,
                     "trace": None,
                     "item_index": None,
                     "import_segments": None,
