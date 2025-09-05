df0a713a4a6f7866fe7f331b719883659e2549a7
Harsh Arora <harsh@chainscore.finance>
2025-09-05 13:45:46 +0530

    feat: implement on-transfer functionality

--- jam/types/state/accumulation/types.py
+++ jam/types/state/accumulation/types.py
@@ -33,11 +33,11 @@ class OperandTuples(TypedVector[OperandTuple]):
 
 @structure
 class DeferredTransfer:
-    sender: ServiceId
-    receiver: ServiceId
-    amount: Balance
-    memo: Bytes
-    gas: Gas
+    sender: ServiceId # s
+    receiver: ServiceId # d
+    amount: Balance # a
+    memo: Bytes # m
+    gas: Gas # g
 
 
 class DeferredTransfers(TypedVector[DeferredTransfer]):
