ffc9e4f1c5868b38d79af442aaf77903ac6f0de1
Kartik213 <kartikg02013@gmail.com>
2025-09-10 17:28:47 +0530

    fix:best block handler, sync handler and parameters updated

--- jam/api/rpc/ws_handlers.py
+++ jam/api/rpc/ws_handlers.py
@@ -17,47 +17,12 @@ from jam.types.protocol.core import CoreIndex
 from jam.types.work.manifest import Extrinsics, Extrinsic
 from jam.types.work.package import WorkPackage
 from jam.network.protocols.ce_133 import WorkPackageSubmission, CE133Data, WorkPackageCore
+from jam.api.rpc.parameters import parameters
 
 Hash = TypedArray[U8]
 
-def parameters(params: list):
-    return {
-        "V1": {
-            "deposit_per_item": 10,
-            "deposit_per_byte": 1,
-            "deposit_per_account": 100,
-            "core_count": 2,
-            "min_turnaround_period": 19200,
-            "epoch_period": 12,
-            "max_accumulate_gas": 1000000,
-            "max_is_authorized_gas": 50000000,
-            "max_refine_gas": 100000000,
-            "block_gas_limit": 20000000,
-            "recent_block_count": 8,
-            "max_work_items": 16,
-            "max_dependencies": 8,
-            "max_tickets_per_block": 3,
-            "max_lookup_anchor_age": 14400,
-            "tickets_attempts_number": 3,
-            "auth_window": 8,
-            "slot_period_sec": 6,
-            "auth_queue_len": 80,
-            "rotation_period": 4,
-            "max_extrinsics": 128,
-            "availability_timeout": 5,
-            "val_count": 6,
-            "max_authorizer_code_size": 64000,
-            "max_input": 13794305,
-            "max_service_code_size": 4000000,
-            "basic_piece_len": 4,
-            "max_imports": 3072,
-            "segment_piece_count": 1026,
-            "max_report_elective_data": 49152,
-            "transfer_memo_size": 128,
-            "max_exports": 3072,
-            "epoch_tail_start": 10
-        }
-    }
+def parameters_handler(params: list):
+    return {"V1": parameters}
 
 def service_preimage_handler(params):
     hh, sid, pi_hash = parse_data([HeaderHash, ServiceId, OpaqueHash], params)
@@ -67,24 +32,27 @@ def service_preimage_handler(params):
     except Exception as e:
         return None
 
-def best_block(params: list):
+def best_block_handler(params: list):
     from jam.settings import settings
-    best = Finality.load_final(settings.main_db)
+    best = Finality.load_latest(settings.main_db)
     return {"header_hash": list(best.header.hash()), "slot": int(best.header.slot)}
+
+def finalized_block_handler(params: list):
+    from jam.settings import settings
+    final = Finality.load_final(settings.main_db)
+    return {"header_hash": list(final.header.hash()), "slot": int(final.header.slot)}
 
 async def initial_subscribe_service_request(sid, pi_hash, pi_len, finality):
     from jam.settings import settings
     # block = Finality.load_final(settings.main_db) if finality else Finality.load_latest(settings.main_db)
     block = Finality.load_final(settings.main_db)
     state_at_hh = State.load(block.header.hash())
-    print("subscribeServiceRequest called", pi_hash.hex())
     try:
         value = list(state_at_hh.delta[sid].preimages[pi_hash])
     except Exception:
         value = None
 
     method = f"subscribeServiceRequest:{sid}:{list(pi_hash)}:{pi_len}:{finality}"
-
     await broker.publish(method, {"header_hash": list(block.header.hash()), "slot": int(block.header.slot), "value": value})
 
 def service_data_handler(params):
@@ -117,13 +85,18 @@ def submit_preimage_handler(params):
     preimg_store.store(pre_img)
 
 def state_root_handler(params: list):
-    hh = parse_data([HeaderHash], params)
-
-    from jam.settings import settings
-    block = Block.load(hh[0], settings.main_db)
-    new_slot = block.header.slot + 1
-    next_block = Block.load_w_ts(new_slot, settings.main_db)
-    return list(next_block.header.parent_state_root)
+    try:
+        header_hash = parse_data([HeaderHash], params)[0]
+        from jam.settings import settings
+        block = Block.load(header_hash, settings.main_db)
+        from jam.state.state import state
+        if block.header.slot == state.tau:
+            return list(state.root)
+        new_slot = block.header.slot + 1
+        next_block = Block.load_w_ts(new_slot, settings.main_db)
+        return list(next_block.header.parent_state_root)
+    except Exception as e:
+        return None
 
 def beefy_root_handler(params: list):
     hh = parse_data([HeaderHash], params)[0]
@@ -135,23 +108,13 @@ def beefy_root_handler(params: list):
 
     return None
 
-def finalized_block_handler(params: list):
-    from jam.settings import settings
-
-    final = Finality.load_final(settings.main_db)
-    return {"header_hash": list(final.header.hash()), "slot": int(final.header.slot)}
-
 def submit_work_package_handler(params: list):
     ci, encoded_wp, ext = parse_data([CoreIndex, Bytes, Extrinsics], params)
 
     wp, _ = WorkPackage.decode_from(encoded_wp)
 
-    print("Received work package", wp.to_json())
-    print("Received core index", ci)
-    print("Received extrinsics", ext)
-
     # TODO: fix it later
-    ext = Extrinsics([Extrinsic(b"") for i in range(len(wp.items))])
+    ext = Extrinsics([Extrinsic(b"") for _ in range(len(wp.items))])
 
     CE133 = WorkPackageSubmission()
     wc = WorkPackageCore(wp, ci)
@@ -175,8 +138,8 @@ def service_request_handler(params: list):
 
 
 method_map: dict[str, Callable] = {
-    "parameters": parameters,
-    "bestBlock": best_block,
+    "parameters": parameters_handler,
+    "bestBlock": best_block_handler,
     "servicePreimage": service_preimage_handler,
     "serviceData": service_data_handler,
     "submitPreimage": submit_preimage_handler,
