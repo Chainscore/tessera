daea3365a36ca049773442502fc6892e775c1c8a
prasadkumkar <prasad@chainscore.finance>
2025-09-14 21:14:20 +0530

    fix: adjust protocol return types

--- jam/execution/invocations/functions/protocol.py
+++ jam/execution/invocations/functions/protocol.py
@@ -1,5 +1,5 @@
-from typing import Dict, Protocol
-from tsrkit_pvm import Memory, ExecutionStatus
+from typing import Dict, Protocol, Any
+from tsrkit_pvm import ExecutionStatus
 
 
 class InvocationFunctions(Protocol):
@@ -17,7 +17,7 @@ class InvocationFunctions(Protocol):
         return decorator
 
     @classmethod
-    def execute(cls, host_call: int, gas: int, registers: list, memory: Memory, context, args):
+    def execute(cls, host_call: int, gas: int, registers: list, memory: Any, context, args):
         # Fast path for gas check
         if gas < 0:
             return ExecutionStatus.OUT_OF_GAS, gas, registers, memory, context
