1a0f5c27110ff4d0c7b106714ecf6a92ef4c1543
Kartik213 <kartikg02013@gmail.com>
2025-10-06 18:19:24 +0530

    commented assurances section

--- jam/state/state.py
+++ jam/state/state.py
@@ -272,10 +272,10 @@ class State:
                 )
 
                 # TODO: Uncomment it for assurances
-                from jam.operations.handlers.assurer import assurer
-                for ext in block.extrinsic.guarantees:
-                    logger.debug("[ASSURER]: Fetching assigned shard", wr_hash=ext.report.hash().hex())
-                    asyncio.create_task(assurer._req_shard(ext))
+                # from jam.operations.handlers.assurer import assurer
+                # for ext in block.extrinsic.guarantees:
+                #     logger.debug("[ASSURER]: Fetching assigned shard", wr_hash=ext.report.hash().hex())
+                #     asyncio.create_task(assurer._req_shard(ext))
 
                 # TODO: Test Auditing & Refining with PJ
                 # # Start Auditing for new block received
