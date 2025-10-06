0454ac9210b3e68de33349fe53156b24e3dbbad7
Kartik213 <kartikg02013@gmail.com>
2025-10-06 18:16:00 +0530

    fix:changed log type from pvm to node

--- jam/state/state.py
+++ jam/state/state.py
@@ -265,17 +265,17 @@ class State:
                 Finality.set_head(header_hash, _set.main_db)
                 logger.info(
                     "Block imported!",
-                    new_wrs=newly_avail_wrs,
+                    new_wrs=len(newly_avail_wrs),
                     header=header_hash.hex()[:16] + "...",
                     timeslot=self.tau,
                     final_state_root=self.root.hex()[:16] + "...",
                 )
 
                 # TODO: Uncomment it for assurances
-                # from jam.operations.handlers.assurer import assurer
-                # for ext in block.extrinsic.guarantees:
-                #     logger.debug("[ASSURER]: Fetching assigned shard", wr_hash=ext.report.hash().hex())
-                #     asyncio.create_task(assurer._req_shard(ext))
+                from jam.operations.handlers.assurer import assurer
+                for ext in block.extrinsic.guarantees:
+                    logger.debug("[ASSURER]: Fetching assigned shard", wr_hash=ext.report.hash().hex())
+                    asyncio.create_task(assurer._req_shard(ext))
 
                 # TODO: Test Auditing & Refining with PJ
                 # # Start Auditing for new block received
